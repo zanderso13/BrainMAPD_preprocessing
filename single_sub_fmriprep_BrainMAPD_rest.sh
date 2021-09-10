e64cafe56b1f7b637c71787fe487fa0bbb6b3368
Zachary Anderson <zaz3744@quser24.quest.it.northwestern.edu>
2021-09-10 08:33:52 -0500

    slight change to path where data is saved

--- single_sub_fmriprep_BrainMAPD_rest.sh
+++ single_sub_fmriprep_BrainMAPD_rest.sh
@@ -13,4 +13,4 @@ cd /projects/b1108
 pwd
 echo "beginning preprocessing"
 
-singularity run --cleanenv -B /projects/b1108:/projects/b1108 /projects/b1108/software/singularity_images/fmriprep-20.2.3.simg /projects/b1108/data/BrainMAPD/ /projects/b1108/projects/BrainMAPD_preproc/fmriprep participant --participant-label ${1} -t REST --fs-license-file /projects/b1108/software/freesurfer_license/license.txt --fs-no-reconall -w /projects/b1108/projects/BrainMAPD_preproc/work --skip_bids_validation
+singularity run --cleanenv -B /projects/b1108:/projects/b1108 /projects/b1108/software/singularity_images/fmriprep-20.2.3.simg /projects/b1108/data/BrainMAPD/ /projects/b1108/projects/BrainMAPD_preproc participant --participant-label ${1} -t REST --fs-license-file /projects/b1108/software/freesurfer_license/license.txt --fs-no-reconall -w /projects/b1108/projects/BrainMAPD_preproc/work --skip_bids_validation
